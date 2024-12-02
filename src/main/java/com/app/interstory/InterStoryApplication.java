2c2e1726aed7e612dec3e13795bb4d73b9c6d08d
afteryoon <khsso102649@gmail.com>
2024-12-02 17:06:24 +0900

    #01 Init : Create InterStory Project
    querydsl,s3, redis, security... 등 config 설정

---
+++ src/main/java/com/app/interstory/InterStoryApplication.java
@@ -0,0 +1,13 @@
+package com.app.interstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterStoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterStoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/interstory/config/WebSecurityConfig.java
@@ -0,0 +1,54 @@
+package com.app.interstory.config;
+
+import jdk.jfr.Enabled;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Enabled
+@RequiredArgsConstructor
+@Slf4j
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        log.debug(" WebSecurityConfig Start !!! ");
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                )
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(logout -> logout.logoutUrl("/auth/logout")
+                        .invalidateHttpSession(true)
+                        .logoutSuccessUrl("/auth/login")
+                )
+                //403 시 처리
+//                .exceptionHandling(config->{
+//                    config.accessDeniedPage("/error/403");
+//                })
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+}
