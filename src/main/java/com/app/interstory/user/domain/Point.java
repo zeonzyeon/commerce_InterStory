8d600a03692f82c759ae5bf29768199d409fd89a
seunghyun.hwang <dragonray@naver.com>
2024-12-03 16:48:53 +0900

    #7 Feat: Point Entity 추가

---
+++ src/main/java/com/app/interstory/user/domain/Point.java
@@ -0,0 +1,38 @@
+package com.app.interstory.user.domain;
+
+import java.sql.Timestamp;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "point")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class Point {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "point_id")
+	private Long pointId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User userId;
+
+	@CreatedDate
+	@Column(name = "used_at", nullable = false, updatable = false)
+	private Timestamp usedAt;
+
+	@Column(name = "balance", nullable = false)
+	private Long balance;
+
+	@Column(name = "description", nullable = false)
+	private String description;
+}
