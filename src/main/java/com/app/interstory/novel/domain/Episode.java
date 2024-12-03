3cb575ad8e2df217660814bd7f78a8b01a58ffa8
seunghyun.hwang <dragonray@naver.com>
2024-12-03 17:15:44 +0900

    #7 Feat: Episode Entity 추가

---
+++ src/main/java/com/app/interstory/novel/domain/Episode.java
@@ -0,0 +1,55 @@
+package com.app.interstory.novel.domain;
+
+import java.sql.Timestamp;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "episode")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Episode {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "episode_id")
+	private Long episodeId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "novel_id", nullable = false)
+	private Novel novelId;
+
+	@Column(name = "title", nullable = false)
+	private String title;
+
+	@Builder.Default
+	@Column(name = "view_count", nullable = false)
+	private Integer viewCount = 0;
+
+	@Column(name = "published_at", nullable = false, updatable = false)
+	@CreatedDate
+	private Timestamp publishedAt;
+
+	@Column(name = "thumbnail_renamed_filename")
+	private String thumbnailRenamedFilename;
+
+	@Column(name = "thumbnail_url")
+	private String thumbnailUrl;
+
+	@Builder.Default
+	@Column(name = "like_count", nullable = false)
+	private Integer likeCount = 0;
+
+	@Column(name = "content", columnDefinition = "TEXT", nullable = false)
+	private String content;
+
+	@Column(name = "status", nullable = false)
+	private Boolean status;
+}
