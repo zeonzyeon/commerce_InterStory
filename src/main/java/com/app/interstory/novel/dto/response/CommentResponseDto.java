6f075610eef407f19fdccc3607d8fe23703573b2
greenapple <dla16253745@gmail.com>
2024-12-05 15:25:52 +0900

    Fix: 시간 포맷팅 변경

--- src/main/java/com/app/interstory/novel/dto/response/CommentResponseDto.java
+++ src/main/java/com/app/interstory/novel/dto/response/CommentResponseDto.java
@@ -1,6 +1,7 @@
 package com.app.interstory.novel.dto.response;
 
 import java.sql.Timestamp;
+import java.time.format.DateTimeFormatter;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -13,8 +14,16 @@ public class CommentResponseDto {
 	private final String nickname;
 	private final String profileUrl;
 	private final String content;
-	private final Timestamp createdAt;
+	private final String createdAt;
 	private final Integer likeCount;
 	private final Boolean isLiked;
 	private final Boolean status;
+
+	public static String formatTimestamp(Timestamp timestamp) {
+		if (timestamp != null) {
+			return timestamp.toLocalDateTime()
+					.format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
+		}
+		return null;
+	}
 }
