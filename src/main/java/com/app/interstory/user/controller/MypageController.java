8df48c4a1d35d44efc2e3baf3c1bd2d6281effda
Jihyun Jeon <hyuneejj@naver.com>
2024-12-16 13:08:21 +0900

    #50 Feat: 연재한 회차 목록 + 독자 반응 UI

--- src/main/java/com/app/interstory/user/controller/MypageController.java
+++ src/main/java/com/app/interstory/user/controller/MypageController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.app.interstory.novel.service.NovelService;
 import com.app.interstory.user.domain.CustomUserDetails;
 import com.app.interstory.user.dto.response.PointHistoryResponseDTO;
 import com.app.interstory.user.service.MypageService;
@@ -19,10 +20,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MypageController {
 	private final MypageService mypageService;
+	private final NovelService novelService;
 
 	// 회원 정보 수정
 
-	// 포인트 내역
+	// 포인트 내역 페이지
 	@GetMapping("/point-history")
 	public String getPointHistory(@AuthenticationPrincipal CustomUserDetails userDetails,
 		Pageable pageable, Model model) {
@@ -30,4 +32,18 @@ public class MypageController {
 		model.addAttribute("pointHistoryPage", pointHistoryPage);
 		return "mypage/point-history";
 	}
+
+	// 에피소드 리스트 페이지
+	// @GetMapping("/novels/{id}/episodes")
+	// public String showEpisodes(@PathVariable Long id, Model model) {
+	// 	Novel novel = novelService.findById(id);
+	// 	List<Episode> episodes = episodeService.findByNovelId(id);
+	// 	List<Reaction> reactions = reactionService.analyzeByNovelId(id);
+	//
+	// 	model.addAttribute("novel", novel);
+	// 	model.addAttribute("episodes", episodes);
+	// 	model.addAttribute("reactions", reactions);
+	// 	return "episodes/episode-list";
+	// }
+
 }
