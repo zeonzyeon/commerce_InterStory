6a80999dbe5528e92de16fcd38ac018c0a770a81
Jihyun Jeon <hyuneejj@naver.com>
2024-12-17 09:43:32 +0900

    #50 Feat: MypageController

--- src/main/java/com/app/interstory/user/controller/ImageController.java
+++ src/main/java/com/app/interstory/user/controller/ImageController.java
@@ -1,76 +1,59 @@
 package com.app.interstory.user.controller;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
-@RequestMapping("/api/image")
+@RequestMapping("/image")
 @Slf4j
 public class ImageController {
-	private final String uploadPath;
-
-	ImageController(@Value("${file.upload-dir}") String uploadPath) {
-		this.uploadPath = uploadPath;
-	}
-
-	@PostMapping("/upload")
-	public ResponseEntity<Map<String, String>> uploadImage(@RequestParam("file") MultipartFile file) {
-		try {
-			Files.createDirectories(Paths.get(uploadPath));
-
-			// 파일 이름 생성 (중복 방지를 위해 UUID 사용)
-			String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
-
-			String fileExtension = StringUtils.getFilenameExtension(file.getOriginalFilename());
-			if (!isValidImageExtension(fileExtension)) {
-				return ResponseEntity.badRequest().body(Map.of("error", "Invalid file type"));
-			}
-
-			// 저장할 경로 생성
-			Path filePath = Paths.get(uploadPath, fileName);
-
-			// 파일 저장
-			Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
-
-			// 이미지에 접근할 수 있는 URL 생성
-			String imageUrl = "/images/uploaded/" + fileName; // 실제 서버의 URL 구조에 맞게 수정
-
-			return ResponseEntity.ok(Map.of("imageUrl", imageUrl));
-		} catch (IOException e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-				.body(Map.of("error", "Failed to upload image"));
-		}
-	}
-
-	private boolean isValidImageExtension(String extension) {
-		if (extension == null)
-			return false;
-		List<String> validExtensions = Arrays.asList("jpg", "jpeg", "png", "gif");
-		return validExtensions.contains(extension.toLowerCase());
-	}
-
-	@PostMapping("/post")
-	public String postImg(@RequestParam("content") String content) {
-		log.error(content);
-		return "redirect:/";
-	}
+	// private final String uploadPath;
+	//
+	// ImageController(@Value("${file.upload-dir}") String uploadPath) {
+	// 	this.uploadPath = uploadPath;
+	// }
+	//
+	// @PostMapping("/upload")
+	// public ResponseEntity<Map<String, String>> uploadImage(@RequestParam("file") MultipartFile file) {
+	// 	try {
+	// 		Files.createDirectories(Paths.get(uploadPath));
+	//
+	// 		// 파일 이름 생성 (중복 방지를 위해 UUID 사용)
+	// 		String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+	//
+	// 		String fileExtension = StringUtils.getFilenameExtension(file.getOriginalFilename());
+	// 		if (!isValidImageExtension(fileExtension)) {
+	// 			return ResponseEntity.badRequest().body(Map.of("error", "Invalid file type"));
+	// 		}
+	//
+	// 		// 저장할 경로 생성
+	// 		Path filePath = Paths.get(uploadPath, fileName);
+	//
+	// 		// 파일 저장
+	// 		Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+	//
+	// 		// 이미지에 접근할 수 있는 URL 생성
+	// 		String imageUrl = "/images/uploaded/" + fileName; // 실제 서버의 URL 구조에 맞게 수정
+	//
+	// 		return ResponseEntity.ok(Map.of("imageUrl", imageUrl));
+	// 	} catch (IOException e) {
+	// 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+	// 			.body(Map.of("error", "Failed to upload image"));
+	// 	}
+	// }
+	//
+	// private boolean isValidImageExtension(String extension) {
+	// 	if (extension == null)
+	// 		return false;
+	// 	List<String> validExtensions = Arrays.asList("jpg", "jpeg", "png", "gif");
+	// 	return validExtensions.contains(extension.toLowerCase());
+	// }
+	//
+	// @PostMapping("/post")
+	// public String postImg(@RequestParam("content") String content) {
+	// 	log.error(content);
+	// 	return "redirect:/";
+	// }
 }
--- src/main/java/com/app/interstory/user/controller/MypageController.java
+++ src/main/java/com/app/interstory/user/controller/MypageController.java
@@ -1,16 +1,34 @@
 package com.app.interstory.user.controller;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.app.interstory.novel.domain.enumtypes.Sort;
+import com.app.interstory.novel.dto.response.NovelDetailResponseDTO;
 import com.app.interstory.novel.service.NovelService;
 import com.app.interstory.user.domain.CustomUserDetails;
+import com.app.interstory.user.dto.request.UpdateUserRequestDTO;
+import com.app.interstory.user.dto.response.AccountResponseDTO;
+import com.app.interstory.user.dto.response.FavoriteNovelResponseDTO;
+import com.app.interstory.user.dto.response.MyCommentResponseDTO;
+import com.app.interstory.user.dto.response.MyNovelResponseDTO;
+import com.app.interstory.user.dto.response.MypageResponseDTO;
 import com.app.interstory.user.dto.response.PointHistoryResponseDTO;
+import com.app.interstory.user.dto.response.ReadNovelResponseDTO;
+import com.app.interstory.user.dto.response.SettlementResponseDTO;
+import com.app.interstory.user.dto.response.UserCouponResponseDTO;
 import com.app.interstory.user.service.MypageService;
 
 import lombok.RequiredArgsConstructor;
@@ -19,44 +37,129 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class MypageController {
+
 	private final MypageService mypageService;
 	private final NovelService novelService;
 
-	// 회원 정보 수정 페이지
+	// 회원 정보 페이지
+	@GetMapping("/profile")
+	public String getUserProfile(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
+		MypageResponseDTO userProfile = mypageService.getUser(userDetails);
+		model.addAttribute("userProfile", userProfile);
+		return "mypage/my-profile";
+	}
+
+	// 관심 작품 페이지
+	@GetMapping("/favorite-novels")
+	public String getFavoriteNovels(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
+		Page<FavoriteNovelResponseDTO> favoriteNovels = mypageService.getFavoriteNovels(userDetails,
+			Pageable.unpaged());
+		model.addAttribute("favoriteNovels", favoriteNovels.getContent());
+		return "mypage/favorite-novel";
+	}
+
+	// 열람 작품 페이지
+	@GetMapping("/recent-novels")
+	public String getRecentNovels(@AuthenticationPrincipal CustomUserDetails userDetails,
+		@PageableDefault(size = 10) Pageable pageable,
+		Model model) {
+		Page<ReadNovelResponseDTO> recentNovels = mypageService.getReadNovels(
+			userDetails,
+			PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(),
+				org.springframework.data.domain.Sort.by(Sort.NEW_TO_OLD.name()))
+		);
+
+		model.addAttribute("recentNovels", recentNovels.getContent());
+		return "mypage/recent-novel";
+	}
 
 	// 포인트 내역 페이지
 	@GetMapping("/point-history")
-	public String getPointHistory(@AuthenticationPrincipal CustomUserDetails userDetails,
-		Pageable pageable, Model model) {
+	public String getPointHistory(
+		@AuthenticationPrincipal CustomUserDetails userDetails,
+		@RequestParam(defaultValue = "1") int page,
+		@RequestParam(defaultValue = "10") int size,
+		Model model) {
+
+		Pageable pageable = PageRequest.of(page - 1, size);
+
 		Page<PointHistoryResponseDTO> pointHistoryPage = mypageService.getPointHistory(userDetails, pageable);
-		model.addAttribute("pointHistoryPage", pointHistoryPage);
+
+		model.addAttribute("pointHistoryList", pointHistoryPage.getContent());
+		model.addAttribute("currentPage", page);
+		model.addAttribute("totalPages", pointHistoryPage.getTotalPages());
+
 		return "mypage/point-history";
 	}
 
-	// 에피소드 리스트 페이지
-	// @GetMapping("/novels/{id}/episodes")
-	// public String showEpisodes(@PathVariable Long id, Model model) {
-	// 	Novel novel = novelService.findById(id);
-	// 	List<Episode> episodes = episodeService.findByNovelId(id);
-	// 	List<Reaction> reactions = reactionService.analyzeByNovelId(id);
-	//
-	// 	model.addAttribute("novel", novel);
-	// 	model.addAttribute("episodes", episodes);
-	// 	model.addAttribute("reactions", reactions);
-	// 	return "episodes/episode-list";
-	// }
+	// 보유 쿠폰 페이지
+	@GetMapping("/my-coupons")
+	public String getCouponPage(@AuthenticationPrincipal CustomUserDetails userDetails, Pageable pageable,
+		Model model) {
+		Page<UserCouponResponseDTO> coupons = mypageService.getCoupons(userDetails, pageable);
+		model.addAttribute("coupons", coupons.getContent());
+		return "mypage/my-coupons";
+	}
 
-	// 관심 작품 페이지
-	// @GetMapping("/favorite-novels")
-	// public String showFavoriteNovels(Model model) {
-	// 	// 관심 작품 리스트 예시
-	// 	List<FavoriteWorkDto> favoriteWorks = List.of(
-	// 		new FavoriteWorkDto("작품명1", "작가명1", "/images/work1.jpg", 1200, 35, 300, "힐링, 성장"),
-	// 		new FavoriteWorkDto("작품명2", "작가명2", "/images/work2.jpg", 800, 20, 200, "로맨스, 감성"),
-	// 		new FavoriteWorkDto("작품명3", "작가명3", "/images/work3.jpg", 1500, 50, 450, "액션, 판타지")
-	// 	);
-	//
-	// 	model.addAttribute("favoriteWorks", favoriteWorks);
-	// 	return "favoriteWorks";
-	// }
+	// 연재 작품 페이지
+	@GetMapping("/my-novel")
+	public String getMyNovels(@AuthenticationPrincipal CustomUserDetails userDetails,
+		@PageableDefault(size = 10) Pageable pageable,
+		Model model) {
+		// 연재 작품 목록
+		Page<MyNovelResponseDTO> myNovels = mypageService.getMyNovels(userDetails, pageable);
+		model.addAttribute("myNovels", myNovels.getContent());
+
+		// 계좌 정보
+		AccountResponseDTO accountInfo = mypageService.getAccount(userDetails);
+		model.addAttribute("accountInfo", accountInfo);
+
+		// 정산 정보
+		SettlementResponseDTO settlement = mypageService.getSettlement(userDetails);
+		model.addAttribute("settlement", settlement);
+		return "mypage/novel-list";
+	}
+
+	// 댓글 페이지
+	@GetMapping("/my-comments")
+	public String getMyComments(@AuthenticationPrincipal CustomUserDetails userDetails,
+		@PageableDefault(size = 10) Pageable pageable,
+		Model model) {
+		Page<MyCommentResponseDTO> myComments = mypageService.getMyComments(
+			userDetails,
+			PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(),
+				org.springframework.data.domain.Sort.by(Sort.NEW_TO_OLD.name()))
+		);
+		model.addAttribute("myComments", myComments.getContent());
+		model.addAttribute("currentPage", pageable.getPageNumber() + 1);
+		model.addAttribute("totalPages", myComments.getTotalPages());
+		return "mypage/my-comments";
+	}
+
+	// 회원정보 수정 페이지
+	@GetMapping("/edit-profile")
+	public String editProfile(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
+		MypageResponseDTO userInfo = mypageService.getUser(userDetails);
+		model.addAttribute("userInfo", userInfo);
+		return "mypage/edit-profile";
+	}
+
+	// 회원정보 수정 처리
+	@PostMapping("/edit-profile")
+	public String updateProfile(@AuthenticationPrincipal CustomUserDetails userDetails,
+		@ModelAttribute UpdateUserRequestDTO updateUserRequestDTO,
+		RedirectAttributes redirectAttributes) {
+		mypageService.updateUser(userDetails, updateUserRequestDTO);
+		redirectAttributes.addFlashAttribute("successMessage", "회원정보가 성공적으로 수정되었습니다.");
+		return "redirect:/users/edit-profile";
+	}
+
+	// 회차 리스트와 반응 분석 페이지
+	@GetMapping("/novels/{novelId}")
+	public String getEpisodeListAndReaction(@PathVariable Long novelId, Model model) {
+		NovelDetailResponseDTO novelDetail = novelService.readNovel(novelId, Sort.NEW_TO_OLD, PageRequest.of(0, 10));
+
+		model.addAttribute("novelDetail", novelDetail);
+		return "mypage/episode-list";
+	}
 }
