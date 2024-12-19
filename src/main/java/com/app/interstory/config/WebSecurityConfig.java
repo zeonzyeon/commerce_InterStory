87faeaeada0c3d8167e37aca14ea0c3e69d9f18e
afteryoon <khsso102649@gmail.com>
2024-12-19 10:45:26 +0900

    #39 [build] : deploy 수정
    h2-console 관련 빈 제거
    
    docker-redis 설정
    ci-cd 변경
    depoly sell
    도커 확인 및 설치 /
    Docker Compose 설치 확인
    Redis 컨테이너 실행 및 볼륨 설정
    재배포시
    기존 Redis 컨테이너 중지 및 재시작

--- src/main/java/com/app/interstory/config/WebSecurityConfig.java
+++ src/main/java/com/app/interstory/config/WebSecurityConfig.java
@@ -1,13 +1,10 @@
 package com.app.interstory.config;
 
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.*;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -60,9 +57,9 @@ public class WebSecurityConfig {
 		return authenticationConfiguration.getAuthenticationManager();
 	}
 
-	@Bean
-	public WebSecurityCustomizer configure() {
-		return web -> web.ignoring().requestMatchers(toH2Console());
-	}
+	// @Bean
+	// public WebSecurityCustomizer configure() {
+	// 	return web -> web.ignoring().requestMatchers(toH2Console());
+	// }
 
 }
