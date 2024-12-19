6b297cbc4cfd23be38db8c1173fc2d6e7f7b39ea
afteryoon <khsso102649@gmail.com>
2024-12-19 17:59:30 +0900

    #49 [build] : InterStroy v1.0.0.0 릴리즈 통합

--- src/main/java/com/app/interstory/config/WebSecurityConfig.java
+++ src/main/java/com/app/interstory/config/WebSecurityConfig.java
@@ -1,8 +1,5 @@
 package com.app.interstory.config;
 
-import jdk.jfr.Enabled;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,54 +11,59 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import jdk.jfr.Enabled;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
 @Enabled
 @RequiredArgsConstructor
 @Slf4j
 public class WebSecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        log.debug(" WebSecurityConfig Start !!! ");
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		log.debug(" WebSecurityConfig Start !!! ");
 
-        return http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize -> authorize
-                        // .requestMatchers("/h2-console/**").permitAll()
-                        .requestMatchers("/api/auth/login", "/auth/login", "/auth/signup").anonymous()
-                        .requestMatchers("/admin/notices").permitAll()
-                        .requestMatchers("/admin/notices/{noticeId}").permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .anyRequest().permitAll()
-                )
-                .sessionManagement(session -> session  // 추가: 세션 관리 :  동일한 계정으로 다른 기기나 브라우저에서 로그인하면 이전 세션이 만료
-                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-                        .invalidSessionUrl("/auth/login")
-                        .maximumSessions(1)
-                        .maxSessionsPreventsLogin(false)
-                )
-                .formLogin(AbstractHttpConfigurer::disable)
-                .logout(logout -> logout.logoutUrl("/auth/logout")
-                        .invalidateHttpSession(true)
-                        .logoutSuccessUrl("/auth/login")
-                )
-                .build();
-    }
+		return http
+			.csrf(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(authorize -> authorize
+				// .requestMatchers("/h2-console/**").permitAll()
+				.requestMatchers("/api/auth/login", "/auth/login", "/auth/signup").anonymous()
+				.requestMatchers("/admin/notices").permitAll()
+				.requestMatchers("/novels/**").authenticated()
+				.requestMatchers("/admin/notices/{noticeId}").permitAll()
+				.requestMatchers("/admin/**").hasRole("ADMIN")
+				.anyRequest().permitAll()
+			)
+			.sessionManagement(session -> session  // 추가: 세션 관리 :  동일한 계정으로 다른 기기나 브라우저에서 로그인하면 이전 세션이 만료
+				.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+				.invalidSessionUrl("/auth/login")
+				.maximumSessions(1)
+				.maxSessionsPreventsLogin(false)
+			)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.logout(logout -> logout.logoutUrl("/auth/logout")
+				.invalidateHttpSession(true)
+				.logoutSuccessUrl("/auth/login")
+			)
+			.build();
+	}
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+	@Bean
+	public AuthenticationManager authenticationManager(
+		AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
 
-    // @Bean
-    // public WebSecurityCustomizer configure() {
-    // 	return web -> web.ignoring().requestMatchers(toH2Console());
-    // }
+	// @Bean
+	// public WebSecurityCustomizer configure() {
+	// 	return web -> web.ignoring().requestMatchers(toH2Console());
+	// }
 
 }
